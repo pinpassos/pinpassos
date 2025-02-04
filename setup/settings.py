bea08672b4a7282c50003f609c63c3aa8b7bd2ff
Matheus Pinheiro Passos <pinheiropassosm@gmail.com>
2025-02-04 09:18:16 -0300

    chore: Added new configurations to docker media

--- setup/settings.py
+++ setup/settings.py
@@ -138,7 +138,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
